8c95ee6790f44d60964f3df28bd50e7688739909
Mac <bellomac16@gmail.com>
2025-08-20 13:40:39 +0800

    Fixed LatentSync

--- LatentSync/inference.sh
+++ LatentSync/inference.sh
@@ -6,6 +6,6 @@ python -m scripts.inference \
     --inference_steps 20 \
     --guidance_scale 1.5 \
     --enable_deepcache \
-    --video_path "data/ori/video/clip_1_part1_NoMoreBets.mp4" \
-    --audio_path "data/ori/audio/tts_NoMoreBets.wav" \
-    --video_out_path "data/output/lipSync_clip_1_part1_NoMoreBets.mp4"
+    --video_path "assets/demo1_video.mp4" \
+    --audio_path "assets/demo1_audio.wav" \
+    --video_out_path "video_out.mp4"
